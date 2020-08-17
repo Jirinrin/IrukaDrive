d05d45fcd7ea3d001a0841804f10d082e812daf4
Jirinrin <jiri.ei@hotmail.com>
2020-08-17 21:59:22 +0200

    Add tap timing results to a list

--- Assets/Scripts/BeatmapManager.cs
+++ Assets/Scripts/BeatmapManager.cs
@@ -4,19 +4,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class Note
+public struct Note
 {
     public int bar;
     public float beat;
 }
 
 [Serializable]
-public struct Beatmap
+public class Beatmap
 {
     public AudioClip song;
     public float bpm;
     public float beatOffset;
-    [Range(2,4)] public int beatsPerBar;
+    [Range(2,4)] public int beatsPerBar = 4;
     public int barOffset;
     // Expected to already be sorted
     public List<Note> notes;
@@ -32,29 +32,35 @@ public class BeatmapManager : MonoBehaviour
     private float _prevNoteBeat;
     private float _nextNoteBeat;
 
+    private List<float> _noteResults = new List<float>();
+
     private bool _beatmapFinished;
 
     private void Start()
     {
         _songManager = GetComponent<SongManager>();
+        LoadBeatmap();
+    }
+
+    private void LoadBeatmap()
+    {
         _songManager.LoadSong(currentBeatmap);
         _noteCollection = currentBeatmap.notes.GetEnumerator();
         _beatmapFinished = false;
+        _noteResults = new List<float>();
         AdvanceNote();
     }
 
     private void AdvanceNote()
     {
-        Debug.Log("advance");
-        _noteCollection.MoveNext();
-        var note = _noteCollection.Current;
-        if (note == null)
+        if (!_noteCollection.MoveNext())
         {
             _beatmapFinished = true;
             return;
         }
-
+        
         _prevNoteBeat = _nextNoteBeat;
+        var note = _noteCollection.Current;
         _nextNoteBeat = note.bar * currentBeatmap.beatsPerBar + note.beat;
     }
 
@@ -63,8 +69,12 @@ public class BeatmapManager : MonoBehaviour
         if (_beatmapFinished || _songManager.SongPosBeats < _nextNoteBeat)
             return;
         
-        while (!_beatmapFinished && _songManager.SongPosBeats >= _nextNoteBeat)
-            AdvanceNote();
+        // Next note passed!
+        
+        // todo: some check to see if the player registered this note
+
+        do AdvanceNote();
+        while (!_beatmapFinished && _songManager.SongPosBeats >= _nextNoteBeat);
         
         OnNote?.Invoke();
     }
@@ -84,6 +94,7 @@ public class BeatmapManager : MonoBehaviour
         var timingEarly = _nextNoteBeat - snapshot;
         var timing = timingLate < timingEarly ? timingLate : -timingEarly;
         // todo: scale this by the bpm
+        _noteResults.Add(timing);
         OnTapped?.Invoke(timing);
     }
 
