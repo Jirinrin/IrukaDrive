6bfc86d70cd96f0afd6755e4b3c020a4b67424d0
Jiri Swen <jiri.ei@hotmail.com>
2020-11-17 00:47:10 +0100

    Allow editing beatmap scroll speed mod

--- Assets/Scripts/Menu/ScreenControllers/SettingControllers/FloatSetting.cs
+++ Assets/Scripts/Menu/ScreenControllers/SettingControllers/FloatSetting.cs
@@ -1,9 +1,57 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Menu.ScreenControllers.SettingControllers
 {
     public class FloatSetting : MonoBehaviour
     {
+        [SerializeField] private TMP_Text keyField;
+        [SerializeField] private TMP_Text valueField;
+        [SerializeField] private Button plusBtn;
+        [SerializeField] private Button minusBtn;
         
+        private float _modulus;
+        private float _value;
+        private float _min;
+        private float _max;
+        private Action<float> _onChange;
+
+        public void Init(string key, float value, float modulus, float min, float max, Action<float> onChange)
+        {
+            keyField.text = key;
+            _value = value;
+            _modulus = modulus;
+            _min = min;
+            _max = max;
+            _onChange = onChange;
+            UpdateValue();
+        }
+
+        private void UpdateValue(bool init = false)
+        {
+            valueField.text = _value.ToString(CultureInfo.CurrentCulture);
+            if (!init)
+                _onChange(_value);
+        }
+        
+        private void Start()
+        {
+            plusBtn.onClick.AddListener(ClickPlus);
+            minusBtn.onClick.AddListener(ClickMinus);
+        }
+
+        private void ClickPlus()
+        {
+            _value = Mathf.Min(_value + _modulus, _max);
+            UpdateValue();
+        }
+        private void ClickMinus()
+        {
+            _value = Mathf.Max(_value - _modulus, _min);
+            UpdateValue();
+        }
     }
 }
--- Assets/Scripts/Menu/ScreenControllers/SettingsScreen.cs
+++ Assets/Scripts/Menu/ScreenControllers/SettingsScreen.cs
@@ -12,9 +12,12 @@ namespace Menu.ScreenControllers
         
         private void Start()
         {
-            // var setting1 = Instantiate(floatSetting, contentContainer.transform);
-            // contentContainer.
-            // var test = PlayerPrefs.
+            var scrollSpeedModSetting = Instantiate(floatSetting, contentContainer.transform);
+            scrollSpeedModSetting.Init("Scroll Speed", Local.Settings.BeatmapScrollSpeedMod, 0.25f, 0.25f, 4f, f =>
+            {
+                Local.Settings.BeatmapScrollSpeedMod = f;
+                Local.CommitSettings();
+            });
         }
 
         public void BackToMainMenu()
