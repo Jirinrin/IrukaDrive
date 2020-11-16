c25d87c1f820e1dafd49b5b57c769170437c5c3b
Jiri Swen <jiri.ei@hotmail.com>
2020-11-17 00:04:54 +0100

    Expand beatmapScore type

--- Assets/Scripts/Shared/Domain/BeatmapScore.cs
+++ Assets/Scripts/Shared/Domain/BeatmapScore.cs
@@ -1,21 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using Gameplay.Domain;
+using UnityEngine;
 
 namespace Shared.Domain
 {
     [Serializable]
     public class BeatmapScore : IComparable<BeatmapScore>
     {
-        // todo: make score saving more detailed
+        public int score; // Range 0-10000000
+        public int perfects;
+        public int earlies;
+        public int lates;
+        public int misses;
+        public int maxCombo; // todo: implement
         
-        // Range 0-1000000 or sth
-        public float score;
+        public BeatmapScore(IReadOnlyCollection<RuntimeNote> notes)
+        {
+            var perfectNotes = notes.Where(n => n.Result == NoteResult.HitPerfect);
+            var earlyNotes = notes.Where(n => n.Result == NoteResult.HitEarly);
+            var lateNotes = notes.Where(n => n.Result == NoteResult.HitLate);
+            var missNotes = notes.Where(n => n.Result == NoteResult.WrongChar || n.Result == NoteResult.Miss);
+            
+            perfects = perfectNotes.Count();
+            earlies = earlyNotes.Count();
+            lates = lateNotes.Count();
+            misses = missNotes.Count();
+            
+            var scoreNormalized = (perfects + (earlies + lates) * .5f) / notes.Count;
+            score = Mathf.FloorToInt(scoreNormalized * 10000000);
+        }
         
         public int CompareTo(BeatmapScore other) => score.CompareTo(other.score);
 
-        public override string ToString()
-        {
-            return score.ToString(CultureInfo.CurrentCulture);
-        }
+        public override string ToString() => score.ToString(CultureInfo.CurrentCulture);
     }
 }
