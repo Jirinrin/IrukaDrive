71832a20908266d95ffad8c3d18e37383667dbd6
Jiri Swen <jiri.ei@hotmail.com>
2020-12-14 22:18:05 +0100

    Make title screen more fancy

---
+++ Assets/Scripts/Menu/ScreenControllers/TitleScreen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Shared.Domain;
+using UnityEngine;
+
+namespace Menu.ScreenControllers
+{
+    public class TitleScreen : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer wheel;
+
+        [SerializeField] private float wheelRotateSpeed;
+
+        public void Update()
+        {
+            wheel.transform.rotation = Quaternion.Euler(new Vector3(0, 0, Time.time * wheelRotateSpeed));
+        }
+    }
+}
