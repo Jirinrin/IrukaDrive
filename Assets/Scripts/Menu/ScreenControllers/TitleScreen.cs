35bb8d7a71d492e99b7bacde0c890a1b2b491f46
Jiri Swen <jiri.ei@hotmail.com>
2020-12-18 14:05:19 +0100

    Make title screen a bit fancier with anim

--- Assets/Scripts/Menu/ScreenControllers/TitleScreen.cs
+++ Assets/Scripts/Menu/ScreenControllers/TitleScreen.cs
@@ -7,12 +7,16 @@ namespace Menu.ScreenControllers
     public class TitleScreen : MonoBehaviour
     {
         [SerializeField] private SpriteRenderer wheel;
+        [SerializeField] private GameObject irukaWrapper;
 
         [SerializeField] private float wheelRotateSpeed;
+        [SerializeField] private float irukaBobAmp;
+        [SerializeField] private float irukaBobSpeed;
 
         public void Update()
         {
             wheel.transform.rotation = Quaternion.Euler(new Vector3(0, 0, Time.time * wheelRotateSpeed));
+            irukaWrapper.transform.rotation = Quaternion.Euler(new Vector3(0, 0, irukaBobAmp * Mathf.Sin(Time.time * irukaBobSpeed)));
         }
     }
 }
