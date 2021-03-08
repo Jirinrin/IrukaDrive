62ec75244b2149d28bcad78855837067e5c8fff4
Jiri Swen <jiri.ei@hotmail.com>
2021-03-08 19:02:50 +0100

    Support beatOffset in EditorWaveForm

--- Assets/Scripts/BeatmapEditor/SingletonComponents/EditorWaveform.cs
+++ Assets/Scripts/BeatmapEditor/SingletonComponents/EditorWaveform.cs
@@ -14,8 +14,8 @@ namespace BeatmapEditor.SingletonComponents
         private bool _active;
 
         private List<float> _samples;
-        private AudioClip _audioClip;
         private float _beatsPerSample;
+        private Song _currentSong;
 
         public void LoadSong(Song song)
         {
@@ -26,16 +26,16 @@ namespace BeatmapEditor.SingletonComponents
                 return;
             }
 
-            _audioClip = song.audio;
+            _currentSong = song;
 
             // it's important to multiply by the number of channels!
-            var samplesPerBeat = (_audioClip.samples * _audioClip.channels) / (_audioClip.length * song.BeatsPerSec);
+            var samplesPerBeat = (song.audio.samples * song.audio.channels) / (song.audio.length * song.BeatsPerSec);
             _beatsPerSample = 1f / samplesPerBeat;
 
             _active = true;
 
-            var samples = new float[_audioClip.samples * _audioClip.channels];
-            _audioClip.GetData(samples, 0);
+            var samples = new float[song.audio.samples * song.audio.channels];
+            song.audio.GetData(samples, 0);
 
             _samples = samples.Where((s, i) => i % EveryHowManySamples == 0).ToList();
         }
@@ -52,13 +52,15 @@ namespace BeatmapEditor.SingletonComponents
             Draw.Color = new Color(.3f,.3f,.3f);
             Draw.PolylineJoins = PolylineJoins.Round;
 
+            var xOffset = _currentSong.beatOffset * _currentSong.BeatsPerSec * EditorTrack.viewState.beatSpacing * -1;
+
             var xPerSample = _beatsPerSample * EditorTrack.viewState.beatSpacing * EveryHowManySamples;
             var samplesPerX = 1f / xPerSample;
-            var sampleOffset = Mathf.FloorToInt(Mathf.Max(samplesPerX * EditorTrack.viewState.panX, 0));
+            var sampleOffset = Mathf.FloorToInt(Mathf.Max(samplesPerX * (EditorTrack.viewState.panX - xOffset), 0));
             var numberOfSamples = Mathf.FloorToInt(Mathf.Min(samplesPerX * EditorTrack.Instance.containerRect.width, Mathf.Max(_samples.Count - sampleOffset, 0f)));
 
             using var p = new PolylinePath();
-            var points = _samples.GetRange(sampleOffset, numberOfSamples).Select((s, i) => new PolylinePoint(new Vector2(xPerSample*(i+sampleOffset), s * 30f)));
+            var points = _samples.GetRange(sampleOffset, numberOfSamples).Select((s, i) => new PolylinePoint(new Vector2(xPerSample*(i+sampleOffset) + xOffset, s * 30f)));
             p.AddPoints(points);
             Draw.Polyline(p);
         }
