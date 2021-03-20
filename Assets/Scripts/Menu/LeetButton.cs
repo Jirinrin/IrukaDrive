d6f306204555a84c74e9f3af92b3aeeb93635390
Jiri Swen <jiri.ei@hotmail.com>
2021-03-20 14:20:58 +0100

    Make leet buttons nicer

---
+++ Assets/Scripts/Menu/LeetButton.cs
@@ -0,0 +1,77 @@
+using System;
+using DG.Tweening;
+using TMPro;
+using Tools;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Menu
+{
+    [RequireComponent(typeof(Button))]
+    [RequireComponent(typeof(Animator))]
+    public class LeetButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        private const float HoverDur = .4f;
+        private static readonly AnimationCurve BlinkCurve = new AnimationCurve(
+            new Keyframe(0f, 1f),
+            new Keyframe(.06f, 0f),
+            new Keyframe(.06f, 1f),
+            new Keyframe(.16f, 0f),
+            new Keyframe(.16f, 1f),
+            new Keyframe(.34f, 0f),
+            new Keyframe(.34f, 1f)
+            );
+        private static readonly int HoverKey = Animator.StringToHash("Hover");
+
+        [SerializeField] private Color textColor = Color.green;
+        [SerializeField] private Color textHoverColor = Color.white;
+
+        private Button _button;
+        private TextMeshProUGUI _text;
+        private Animator _anim;
+
+        private bool _hovering;
+        private float _hoverT;
+
+        private void HoverTransition(float newT)
+        {
+            _hoverT = newT;
+            var tt = _hoverT * HoverDur;
+            var a = Interp.BlinkDownUp(.01f, .05f, tt) * Interp.BlinkDownUp(.13f, .05f, tt) * Interp.BlinkDownUp(.35f, .01f, tt);
+            _text.color = Color.Lerp(textColor, textHoverColor, _hoverT).SetAlpha(a);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _hovering = true;
+            // Cursor.SetCursor()
+            _hoverT = 0f;
+            DOTween.To(() => _hoverT, HoverTransition, 1f, HoverDur)
+                .OnComplete(() => _anim.SetBool(HoverKey, _hovering));
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _hovering = false;
+            _text.color = textColor;
+            _anim.SetBool(HoverKey, false);
+            // t = 1f;
+            // _text.DOColor(textColor, HoverDur);
+            // DOTween.To(() => t, HoverTransition, 0f, HoverDur);
+        }
+
+        private void OnDisable()
+        {
+            _text.color = textColor;
+        }
+        private void OnEnable()
+        {
+            _text = GetComponentInChildren<TextMeshProUGUI>();
+            _text.color = textColor;
+            _button = GetComponent<Button>();
+            _anim = GetComponent<Animator>();
+            _anim.SetBool(HoverKey, false);
+        }
+    }
+}
---
+++ Assets/Scripts/Tools/Interp.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tools
+{
+    public static class Interp
+    {
+        public static float LinearStep(float edge0, float edge1, float x) =>
+            Mathf.Clamp((x - edge0) / (edge1 - edge0), 0, 1);
+
+        public static float SmoothStep(float edge0, float edge1, float x) {
+            var t = LinearStep(edge0, edge1, x);
+            return t * t * (3f - 2f * t);
+        }
+        public static float ToSms(float lnsNumber) => SmoothStep(0, 1, lnsNumber);
+
+        public static float SmootherStep(float edge0, float edge1, float x)
+        {
+            // Scale, and clamp x to 0..1 range
+            var t = LinearStep(edge0, edge1, x);
+            // Evaluate polynomial
+            return t * t * t * (t * (t * 6f - 15f) + 10f);
+        }
+
+        public static float EaseInStep(float edge0, float edge1, float x) {
+            var t = LinearStep(edge0, edge1, x);
+            return t * t;
+        }
+
+        public static float EaseOutStep(float edge0, float edge1, float x) =>
+            Mathf.Clamp(SmoothStep(edge0-(edge1-edge0), edge1, x) * 2 - 1, 0,1);
+        public static float ToEos(float lnsNumber) => EaseOutStep(0, 1, lnsNumber);
+
+        public static float Sms(float edge0, float transitionWidth, float x) =>
+            SmoothStep(edge0, edge0+transitionWidth, x);
+
+        public static float SmsSpike(float top, float edgeDistance, float x) =>
+            SmoothStep(top-edgeDistance, top, x) - SmoothStep(top, top+edgeDistance, x);
+
+        public static float Lns(float edge0, float transitionWidth, float x) =>
+            LinearStep(edge0, edge0+transitionWidth, x);
+
+        public static List<float> SplitLns(float lnsNumber, int[] splitPoints, float x)
+        {
+            var output = new List<float>();
+            for (var i = 0; i < splitPoints.Length-1; i++)
+                output.Add(LinearStep(splitPoints[i], splitPoints[i+1], x));
+            return output;
+        }
+
+        public static int Step(float edge, float x) =>    x >= edge ? 1 : 0;
+        public static int StepExcl(float edge, float x) => x > edge ? 1 : 0;
+
+        public static float BlinkUpDown(float up, float duration, float x) =>
+            Step(up,x) - Step(up+duration,x);
+        public static float BlinkDownUp(float down, float dur, float x) => 1f - BlinkUpDown(down, dur, x);
+    }
+}
